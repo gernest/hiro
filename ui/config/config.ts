0b49bd4a00bd766b02314ae80383af019f0223e5
gernest <geofreyernest@live.com>
2019-08-11 11:38:50 +0300

    use english as the default language

--- ui/config/config.ts
+++ ui/config/config.ts
@@ -19,8 +19,7 @@ const plugins: IPlugin[] = [
       locale: {
         // default false
         enable: true,
-        // default zh-CN
-        default: 'zh-CN',
+        default: 'en-US',
         // default true, when it is true, will use `navigator.language` overwrite default
         baseNavigator: true,
       },
