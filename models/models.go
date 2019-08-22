5b1412f8c815a8e165129a2b5bbf317c4fc7edbf
gernest <geofreyernest@live.com>
2019-08-22 18:57:27 +0300

    use email instead of userName

--- models/models.go
+++ models/models.go
@@ -158,7 +158,7 @@ type ListOptions struct {
 
 // Login json object for login.
 type Login struct {
-	Name     string `json:"userName"`
+	Name     string `json:"email"`
 	Password string `json:"password"`
 }
 
